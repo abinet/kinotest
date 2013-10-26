457d9317801559e746a14c997f8bfe51701da3d1
Andriy Binetskyy <abinet@gmail.com>
2013-10-26 15:31:05 +0200

    created TicketForm

---
+++ kinotest/src/main/java/com/abinet/kinotest/TicketForm.java
@@ -0,0 +1,29 @@
+package com.abinet.kinotest;
+
+public class TicketForm {
+	
+	private String movieId;
+	private String lastName;
+	private String creditCardNumber;
+	
+	public String getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getCreditCardNumber() {
+		return creditCardNumber;
+	}
+	public void setCreditCardNumber(String creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+	
+	
+}
